b7ee7f30a6dec8791616d3edfb4aa8b3067882b2
Rokas <rokas.sturma@gmail.com>
2025-04-11 11:36:11 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Mini-localStorage-CRUD-Animals/',
+  base: '/Mini-localstorage-farm/',
   plugins: [react()],
 })
 
